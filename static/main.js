587712e7e8365937a774020666d77c62c84541b5
Lachlan Spencer <spencerlachlan1@gmail.com>
2026-01-07 13:59:21 +1100

    Updated

--- static/main.js
+++ static/main.js
@@ -1,1 +1,17 @@
-const
+
+// define veriables
+const buttons = document.querySelectorAll(".time-controls_button");
+var time = 8;
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    console.log("Clicked score:", button.dataset.level);
+    time = button.dataset.score;
+
+    // Remove the active class from all buttons
+    buttons.forEach(btn => btn.classList.remove("time-controls_button--active"));
+
+    // Add the active class to the clicked button
+    button.classList.add("time-controls_button--active");
+  });
+});
