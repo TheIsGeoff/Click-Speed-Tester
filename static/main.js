5b8519fbdb609f1f94a8b0c9a3b4214117dcf963
Lachlan Spencer <spencerlachlan1@gmail.com>
2026-01-07 16:09:02 +1100

    Updated

--- static/main.js
+++ static/main.js
@@ -5,8 +5,10 @@ const buttons = document.querySelectorAll(".time-controls_button");
 const mainButton = document.getElementById("clicker-input_button");
 const timerDisplay = document.getElementById("timer-display");
 const timeControls = document.getElementById("time-controls");
+const resetButton = document.getElementById("reset_button");
 var time = 8;
 var clicks = 0
+var gameActive = 1
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
@@ -22,7 +24,7 @@ buttons.forEach(button => {
 });
 
 mainButton.addEventListener("click", () => {
-    if (clicks === 0) {
+    if (clicks === 0 & gameActive === 1) {
         clicks = 1;
         timerDisplay.classList.remove("hidden");
         timerDisplay.innerHTML = `<p>${time}.00</p>`;
@@ -30,21 +32,16 @@ mainButton.addEventListener("click", () => {
         timeControls.classList.add("hidden");
         UpdateButtonText(clicks);
         StartTimer(time);
-    } else {
+    } else if (gameActive === 1) {
         clicks += 1;
         UpdateButtonText(clicks);
     }
-
 });
-
-function UpdateButtonText(value) {
-    mainButton.innerHTML = `<h1 id="button-text">${value}</h1>`;
-}
 
 function StartTimer(duration) {
     let remainingTime = duration;
 
-    timerDisplay.innerHTML = `<p>${remainingTime}</p>`;
+    timerDisplay.innerHTML = `<img src="/static/icons/alarm-clock-regular-full.svg" alt="" class="icon-img"><p>${remainingTime}</p>`;
 
     const timerInterval = setInterval(() => {
         remainingTime -= 1;
@@ -52,8 +49,32 @@ function StartTimer(duration) {
         if (remainingTime <= 0) {
             remainingTime = 0;
             clearInterval(timerInterval);
+            gameActive = 0;
+            endGame();
         }
 
-        timerDisplay.innerHTML = `<p>${remainingTime}</p>`;
+        timerDisplay.innerHTML = `<img src="/static/icons/alarm-clock-regular-full.svg" alt="" class="icon-img"><p>${remainingTime}</p>`;
     }, 1000);
+}
+
+resetButton.addEventListener("click", () => {
+    // Reset variables
+    clicks = 0;
+    gameActive = 1;
+
+    // Reset UI elements
+    timerDisplay.classList.add("hidden");
+    timeControls.classList.remove("hidden");
+    resetButton.classList.add("hidden");
+    UpdateButtonText("CLICK TO BEGIN!");
+});
+
+function endGame() {
+    timerDisplay.innerHTML = `<img src="/static/icons/alarm-clock-regular-full.svg" alt="" class="icon-img"><p>${time} second test:</p>`;
+    UpdateButtonText(`Your CPS Is ${ (clicks / parseInt(time)).toFixed(2) }`);
+    resetButton.classList.remove("hidden");
+};
+
+function UpdateButtonText(value) {
+    mainButton.innerHTML = `<h1 id="button-text">${value}</h1>`;
 }
